42c4c7d509e6596ae5fccfa0cd3a5e679b179b9b
Susumu Yamazaki <zacky1972@gmail.com>
2021-06-19 08:16:11 +0900

    create description of PelemayNxBridge

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PelemayNxBridge
 
-**TODO: Add description**
+PelemayNxBridge is an experimental code that will provide PelemayNxBridge.Enum compatible with Elixir.Enum. It uses Nx as the underlying data structure and redefines Elixir's general-purpose list, map, and other data structures. In the future, we plan to incorporate it into Elixir and Erlang, and develop it so that it will be accelerated and parallelized by BeamAsm.
 
 ## Installation
 
--- lib/pelemay_nx_bridge.ex
+++ lib/pelemay_nx_bridge.ex
@@ -1,5 +1,10 @@
 defmodule PelemayNxBridge do
   @moduledoc """
-  Documentation for `PelemayNxBridge`.
+  `PelemayNxBridge` is an experimental code that will provide `PelemayNxBridge.Enum` 
+  compatible with `Elixir.Enum`. 
+  It uses Nx as the underlying data structure and redefines Elixir's general-purpose 
+  list, map, and other data structures. 
+  In the future, we plan to incorporate it into Elixir and Erlang, 
+  and develop it so that it will be accelerated and parallelized by BeamAsm.
   """
 end
